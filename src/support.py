3815d0a2320846def1679afd2161ff41686352bb
Paula Lobo Ferreira <65886945+paulakaferreira@users.noreply.github.com>
2023-08-10 21:57:02 +0200

    Update src/support.py
    Co-authored-by: sprezz-arthur <120720568+sprezz-arthur@users.noreply.github.com>

--- src/support.py
+++ src/support.py
@@ -4,7 +4,8 @@ import os
 def import_folder(path):
     sprite_list = []
     for folder_name, sub_folder, contents in os.walk(path):
-        for content in contents:
+        # TODO: this assumes sprites are ordered alphabetically, which is not true for numbered sprites beyond 9
+        for content in sorted(contents):
             full_path = path + '/' + content
             # imports the image as a surface
             content_surf = pygame.image.load(full_path).convert_alpha()
